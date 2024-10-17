5de62626821d0f72f728dc36ca29e0d51c692a37
Marcos <dev.marcosanjos@gmail.com>
2024-10-16 21:32:55 -0300

    feat: cleanup nextjs styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,11 +8,11 @@ const NotFoundPage = async () => {
           <h1 className='text-primary-600 dark:text-primary-500 mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl'>
             404
           </h1>
-          <p className='mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl dark:text-white'>
+          <p className='mb-4 text-3xl font-bold tracking-tight text-black md:text-4xl'>
             Página não encontrada
           </p>
 
-          <a href='/' className='flex rounded-md bg-black px-4 py-2 text-white'>
+          <a href='/' className='flex justify-center rounded-md bg-black px-4 py-2 text-white'>
             Voltar para página inicial
           </a>
         </div>
