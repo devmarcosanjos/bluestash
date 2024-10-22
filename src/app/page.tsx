10aef1db27f24b97bb9fcb08f0ad3f7ee562d808
Nill <niltonrck@gmail.com>
2024-10-22 16:46:31 -0300

    feat: remove get user data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import {Header, Hero} from '@/app/_components'
-import {loadUserData} from '@/server/functions/user.function'
 
 export default async function Home() {
-  const res = await loadUserData()
-
-  console.log({res})
-
   return (
     <div className='flex h-screen w-full flex-col items-center'>
       <Header />
