2e2352a4c4b48edc28d28810136f2b7a8f897d04
Nill <niltonrck@gmail.com>
2024-10-24 17:33:56 -0300

    feat: adding padding

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className='flex min-h-screen w-full flex-col '>
       <div className='flex h-screen w-full flex-col items-center'>
         <Header />
-        <div className='w-full flex-1 bg-base-200 pt-10'>
+        <div className='w-full flex-1 bg-base-200 p-10'>
           <div className='mx-auto w-full py-5 md:p-10 md:py-0 lg:w-1/2'>
             <h1 className='text-center text-2xl font-semibold text-black sm:text-3xl'>
               Contact us
