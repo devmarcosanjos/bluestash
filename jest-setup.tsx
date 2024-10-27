2a84bf8acac8448e5cef4a2822a1d6da9aa75fc1
Nill <niltonrck@gmail.com>
2024-10-27 20:51:56 -0300

    test: ensure to return a list of todos

--- jest-setup.tsx
+++ jest-setup.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom'
-import {configure} from 'mobx'
+import { configure } from 'mobx'
 import fetchMock from 'jest-mock-fetch'
 
-configure({enforceActions: 'never'})
-
+configure({ enforceActions: 'never' })
 global.fetch = fetchMock as any
