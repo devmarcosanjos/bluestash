7b3cd8119958c76222d9b81ba73d57e4d4f315c1
Marcos <dev.marcosanjos@gmail.com>
2024-11-19 09:55:32 -0300

    feat: integrate zod for form validation in ButtonNewTask component

--- src/app/admin/_components/button-new-task/button-new-task.tsx
+++ src/app/admin/_components/button-new-task/button-new-task.tsx
@@ -1,12 +1,25 @@
 'use client'
 
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
+import { z } from 'zod'
 import { PlusIcon } from 'lucide-react'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { merge } from '@/utils'
 import { DatePicker } from '@/app/admin/_components/date-picker'
 import { ClickOutsideDetector } from '@/app/admin/_components/click-outside-detector'
+
+const taskSchema = z.object({
+  task: z.string().min(1, 'Campo obrigatório'),
+  list: z.enum(['Todo List', 'Work', 'Frella'], { message: 'Campo obrigatório' }),
+  priority: z.enum(['1', '2', '3'], { message: 'Campo obrigatório' }),
+  date: z.date(),
+  notes: z.string().optional(),
+})
+
+type TaskSchema = z.infer<typeof taskSchema>
 
 interface ButtonNewTaskProps {
   showNewTask: Date
@@ -15,6 +28,24 @@ interface ButtonNewTaskProps {
 
 export const ButtonNewTask = ({ showNewTask, setShowNewTask }: ButtonNewTaskProps) => {
   const [open, setOpen] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    getValues,
+    formState: { errors, isValid },
+  } = useForm<TaskSchema>({
+    resolver: zodResolver(taskSchema),
+    defaultValues: {
+      date: showNewTask,
+    },
+  })
+
+  function handleSaveTask(data: TaskSchema) {
+    console.log(data)
+  }
+
   return (
     <div className={merge(['dropdown dropdown-end dropdown-top', open && 'dropdown-open'])}>
       <div role='button' className='flex items-center hover:cursor-pointer'>
@@ -27,23 +58,38 @@ export const ButtonNewTask = ({ showNewTask, setShowNewTask }: ButtonNewTaskProp
       </div>
 
       <ClickOutsideDetector onClickOutside={() => setOpen(false)}>
-        <form>
+        <form onSubmit={handleSubmit(handleSaveTask)}>
           <ul className='menu dropdown-content z-[1] w-[500px] rounded-lg border border-gray-200 bg-white p-4 shadow-lg'>
             <li className='mb-3'>
-              <label className='input flex items-center gap-2 rounded-lg border bg-gray-50 px-3 py-2'>
+              <label
+                className={merge([
+                  'input flex items-center gap-2 rounded-lg border bg-gray-50 px-3 py-2',
+                  errors.task && 'border-red-500',
+                ])}>
                 <PlusIcon size={18} className='text-gray-400' />
                 <input
                   type='text'
                   placeholder='Create new task'
                   className='flex-1 bg-transparent text-gray-700 outline-none'
+                  {...register('task')}
                 />
               </label>
             </li>
 
             <div className='mb-3 flex gap-3'>
+              <label htmlFor='list' className='sr-only'>
+                Selecionar lista
+              </label>
               <li className=''>
-                <select className='select select-bordered flex w-full items-center rounded-lg bg-gray-50 text-gray-700'>
-                  <option disabled selected>
+                <select
+                  id='list'
+                  defaultValue=''
+                  className={merge([
+                    'select select-bordered flex w-full items-center rounded-lg bg-gray-50 text-gray-700',
+                    errors.list && 'border-red-500',
+                  ])}
+                  {...register('list')}>
+                  <option disabled value=''>
                     Lista
                   </option>
                   <option>Todo List</option>
@@ -52,28 +98,53 @@ export const ButtonNewTask = ({ showNewTask, setShowNewTask }: ButtonNewTaskProp
                 </select>
               </li>
               <li className=''>
-                <select className='select select-bordered w-full rounded-lg bg-gray-50 text-gray-700'>
-                  <option disabled selected>
+                <select
+                  defaultValue=''
+                  className={merge([
+                    'select select-bordered w-full rounded-lg bg-gray-50 text-gray-700',
+                    errors.priority && 'border-red-500',
+                  ])}
+                  {...register('priority')}>
+                  <option disabled value=''>
                     Prioridade
                   </option>
-                  <option>Baixa</option>
-                  <option>Média</option>
-                  <option>Alta</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
                 </select>
               </li>
-              <DatePicker value={showNewTask} onChange={setShowNewTask} />
+              <DatePicker
+                value={getValues('date') || showNewTask}
+                onChange={date => {
+                  setValue('date', date)
+                  setShowNewTask(date)
+                }}
+              />
             </div>
 
             <li className='mb-3'>
               <textarea
                 rows={5}
                 placeholder='Add notes'
-                className='textarea w-full resize-none rounded-lg bg-gray-50 text-gray-700'></textarea>
+                className='textarea w-full resize-none rounded-lg bg-gray-50 text-gray-700'
+                {...register('notes')}></textarea>
             </li>
 
             <div className='mt-4 flex justify-end gap-2'>
-              <button className='btn btn-success rounded-lg bg-success px-4 py-2'>Save</button>
-              <button className='btn btn-error rounded-lg bg-error px-4 py-2 '>Reset</button>
+              <button
+                type='submit'
+                disabled={!isValid}
+                className='btn btn-success rounded-lg bg-success px-4 py-2'>
+                Save
+              </button>
+              <button
+                className='btn btn-error rounded-lg bg-error px-4 py-2'
+                onClick={() => {
+                  reset()
+                  setOpen(false)
+                }}>
+                Reset
+              </button>
             </div>
           </ul>
         </form>
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,8 +2,6 @@
 
 import { useState } from 'react'
 
-import { ClockIcon, EllipsisVerticalIcon } from 'lucide-react'
-
 import { ButtonNewTask, TodoList } from '@/app/admin/_components'
 import { DatePickerButton } from '@/app/admin/_components/date-picker-button'
 
@@ -15,26 +13,9 @@ export default function Page() {
     <div className='flex flex-col gap-2'>
       <div className='flex items-end justify-between pt-6'>
         <h1 className='text-3xl font-bold leading-[1.875rem]'>TodoList</h1>
-
         <DatePickerButton value={date} onChange={setDate} />
       </div>
       <TodoList />
-
-      <div className='flex items-center gap-2 rounded-lg bg-base-200 p-3'>
-        <input type='checkbox' className='checkbox-primary checkbox checkbox-sm' />
-        <span className='flex-grow'>Texto ....</span>
-        <div className='flex items-center gap-2'>
-          <ClockIcon size={18} />
-          <span>08:00</span>
-          <span>-</span>
-          <span>09-00</span>
-        </div>
-        <button className='btn btn-square btn-sm bg-base-100'>
-          <span>
-            <EllipsisVerticalIcon size={18} className='text-base-content' />
-          </span>
-        </button>
-      </div>
 
       <div className='absolute bottom-4 left-0 flex  w-full justify-center overflow-visible'>
         <ButtonNewTask showNewTask={showNewTask} setShowNewTask={setShowNewTask} />
