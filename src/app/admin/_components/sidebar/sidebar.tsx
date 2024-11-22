7db55f5de19b90cba25cf1e00c9c705c92913a4c
Marcos <dev.marcosanjos@gmail.com>
2024-11-21 23:53:37 -0300

    feat: fetch and display categories in Sidebar component

--- src/app/admin/_components/sidebar/sidebar.tsx
+++ src/app/admin/_components/sidebar/sidebar.tsx
@@ -1,4 +1,6 @@
-// import SidebarButton from './sidebar-button'
+'use client'
+
+import { useEffect, useState } from 'react'
 
 import Link from 'next/link'
 import Image from 'next/image'
@@ -6,9 +8,25 @@ import Image from 'next/image'
 import { Settings } from 'luxon'
 import { ListCheck, PlusIcon } from 'lucide-react'
 
+import { categoryApi } from '@/apis/category.api'
+import { CategoryModel } from '@/types/models/category.model'
+
 Settings.defaultLocale = 'pt-BR'
 
 const Sidebar = () => {
+  const [category, setCategory] = useState<CategoryModel[]>([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await categoryApi.getAllCategory()
+        setCategory(data)
+      } catch (error) {
+        console.error('Erro ao buscar categorias:', error)
+      }
+    }
+    getData()
+  }, [])
   return (
     <div className='m-2 w-64 rounded-lg bg-base-200'>
       <div className='navbar-start flex w-full items-center justify-center px-6 pb-10 pt-6'>
@@ -18,15 +36,27 @@ const Sidebar = () => {
       </div>
 
       <div className='flex w-full flex-col gap-2 px-6'>
-        <Link href='/admin' className='flex w-full'>
+        {category.map((item, index) => (
+          <Link href='/admin' key={item.id || index} className='flex w-full'>
+            <button className='btn flex-1 border-none bg-secondary shadow-none'>
+              <div className='flex flex-grow items-center gap-2'>
+                <ListCheck size={18} />
+                <span className='font-light'>{item.name}</span>
+              </div>
+              <div className='badge badge-secondary'>+99</div>
+            </button>
+          </Link>
+        ))}
+
+        {/* <Link href='/admin' className='flex w-full'>
           <button className='btn flex-1 border-none bg-secondary shadow-none'>
             <div className='flex flex-grow items-center gap-2'>
               <ListCheck size={18} />
-              <span className='font-light '>TodoList</span>
+              <span className='font-light'>TodoList</span>
             </div>
             <div className='badge badge-secondary'>+99</div>
           </button>
-        </Link>
+        </Link> */}
 
         <Link href='#' className='flex w-full'>
           <button className='btn btn-primary flex-1'>
