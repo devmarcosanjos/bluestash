cccc2e1ae120f08ae3e60d5612212b809a697f28
marcos <dev.marcosanjos@gmail.com>
2024-12-01 19:58:01 -0300

    feat: integrate react-toastify for success notifications and update layout for toast container

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { ToastContainer } from 'react-toastify'
 
 import { Metadata } from 'next'
 
@@ -6,6 +7,7 @@ import { HeaderAdmin } from '@/app/admin/_components'
 import Sidebar from '@/app/admin/_components/sidebar/sidebar'
 import { getCurrentUser } from '@/server/functions/user.function'
 
+import 'react-toastify/dist/ReactToastify.css'
 export const metadata: Metadata = {
   description: '',
   title: 'Admin',
@@ -20,6 +22,8 @@ export default async function Layout({ children }: Props) {
 
   return (
     <div className='flex h-screen w-auto flex-row '>
+      <ToastContainer position='top-right' />
+
       <Sidebar />
       <div className='relative mx-auto max-w-7xl flex-1  px-5'>
         <HeaderAdmin user={user} />
