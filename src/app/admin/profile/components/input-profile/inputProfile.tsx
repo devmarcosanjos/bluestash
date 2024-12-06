add9cea804b30e38ab2b0f3da7f52860d0dc93af
marcos <dev.marcosanjos@gmail.com>
2024-12-05 21:05:02 -0300

    feat: add account deletion functionality with confirmation modal in profile input component

--- src/app/admin/profile/components/input-profile/inputProfile.tsx
+++ src/app/admin/profile/components/input-profile/inputProfile.tsx
@@ -6,30 +6,44 @@ import { UserModel } from '@/types/models'
 
 type Props = {
   user: UserModel
+  onAccountDeleted: () => void
 }
 
-const InputProfile = ({ user }: Props) => {
+const InputProfile = ({ user, onAccountDeleted }: Props) => {
   const [name, setName] = useState(user?.name || '')
   const [originalName, setOriginalName] = useState(user?.name || '')
+  const [isModalOpen, setIsModalOpen] = useState(false) // Estado para controlar o modal
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
   }
 
-  const handleSave = (name: any) => {
-    console.log('Name:', name)
+  const handleSave = () => {
     setOriginalName(name)
   }
 
   const isNameChanged = name !== originalName
 
+  const handleDeleteAccount = () => {
+    setIsModalOpen(true)
+  }
+
+  const confirmDeleteAccount = () => {
+    onAccountDeleted()
+    setIsModalOpen(false)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
   return (
-    <div className=' w-full rounded-lg bg-white p-8'>
-      <div className='flex w-full flex-col gap-4'>
-        <label className='form-control w-full'>
-          <div className='label'>
-            <span className='label-text'>Nome completo</span>
-          </div>
+    <div className='w-full rounded-lg bg-white p-8 shadow-md'>
+      <div className='flex flex-col gap-6'>
+        <div className='form-control w-full'>
+          <label className='label'>
+            <span className='label-text text-lg font-semibold'>Nome completo</span>
+          </label>
           <input
             type='text'
             value={name}
@@ -37,12 +51,12 @@ const InputProfile = ({ user }: Props) => {
             placeholder='Nome Completo'
             className='input input-bordered w-full'
           />
-        </label>
+        </div>
 
-        <label className='form-control w-full'>
-          <div className='label'>
-            <span className='label-text'>Email</span>
-          </div>
+        <div className='form-control w-full'>
+          <label className='label'>
+            <span className='label-text text-lg font-semibold'>Email</span>
+          </label>
           <input
             readOnly
             type='email'
@@ -50,17 +64,55 @@ const InputProfile = ({ user }: Props) => {
             placeholder='email@email.com'
             className='input input-bordered w-full cursor-not-allowed'
           />
-        </label>
+        </div>
 
-        <div className='mt-6 flex justify-end gap-3'>
+        <div className='mt-6 flex justify-end gap-4'>
           <button
+            onClick={handleSave}
             disabled={!isNameChanged}
-            onClick={() => handleSave(name)}
-            className='btn rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600'>
-            Save
+            className='btn rounded-lg bg-green-500 px-6 py-2 text-white transition-all duration-300 ease-in-out hover:bg-green-600 focus:outline-none'>
+            Salvar
+          </button>
+        </div>
+
+        {/* Botão para excluir conta */}
+        <div className='form-control mt-12 w-full'>
+          <label className='label'>
+            <span className='label-text text-lg font-semibold'>
+              Deseja excluri sua conta e todos seus registros?
+            </span>
+          </label>
+          <button
+            onClick={handleDeleteAccount}
+            className='btn rounded-lg bg-red-500 px-6 py-2 text-white transition-all duration-300 ease-in-out hover:bg-red-600 focus:outline-none'>
+            Excluir Conta
           </button>
         </div>
       </div>
+
+      {/* Modal de confirmação de exclusão */}
+      {isModalOpen && (
+        <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
+          <div className='w-1/3 rounded-lg bg-white p-6 text-center shadow-lg'>
+            <h2 className='mb-4 text-2xl font-semibold text-gray-800'>Tem certeza?</h2>
+            <p className='mb-4 text-gray-600'>
+              Esta ação não pode ser desfeita. Todos os seus dados serão apagados.
+            </p>
+            <div className='flex justify-around'>
+              <button
+                onClick={closeModal}
+                className='btn rounded-md bg-gray-500 px-6 py-2 text-white hover:bg-gray-600 focus:outline-none'>
+                Cancelar
+              </button>
+              <button
+                onClick={confirmDeleteAccount}
+                className='btn rounded-md bg-red-500 px-6 py-2 text-white hover:bg-red-600 focus:outline-none'>
+                Confirmar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
