d12dc8e8d1524028b8a7869857a126e77637de2a
marcos <dev.marcosanjos@gmail.com>
2024-12-26 20:17:07 -0300

    fix: remove account deletion handler from profile layout

--- src/app/admin/profile/layout.tsx
+++ src/app/admin/profile/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 import { Metadata } from 'next'
 
-import { deleteUserAccount, getCurrentUser } from '@/server/functions/user.function'
+import { getCurrentUser } from '@/server/functions/user.function'
 import InputProfile from '@/app/admin/profile/components/input-profile/inputProfile'
 
 export const metadata: Metadata = {
@@ -17,18 +17,18 @@ type Props = {
 export default async function Layout({ children }: Props) {
   const user = await getCurrentUser()
 
-  const handleDeleteAccount = async () => {
-    try {
-      await deleteUserAccount(user.id)
-      console.log('Conta deletada com sucesso', user.id)
-    } catch (error) {
-      console.error('Erro ao deletar conta', error)
-    }
-  }
+  // const handleDeleteAccount = async () => {
+  //   try {
+  //     await deleteUserAccount(user.id)
+  //     console.log('Conta deletada com sucesso', user.id)
+  //   } catch (error) {
+  //     console.error('Erro ao deletar conta', error)
+  //   }
+  // }
 
   return (
     <div>
-      <InputProfile user={user} onAccountDeleted={handleDeleteAccount} />
+      <InputProfile user={user} />
       {children}
     </div>
   )
