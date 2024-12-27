d33185ce1f76c50da056062556e714168f6e50c6
marcos <dev.marcosanjos@gmail.com>
2024-12-26 21:04:56 -0300

    feat: update dashboard page to fetch and display todo statistics dynamically

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
   LucideCalendarCheck,
   LucideCheckCheck,
@@ -8,28 +6,43 @@ import {
   LucideTally5,
 } from 'lucide-react'
 
+import { getDataTodo } from '@/server/data/dashboard.data'
 import SummaryCard from '@/app/admin/dashboard/_components/summary-card'
-import { TodoFormContextProvider } from '@/app/admin/_context/todo.context'
 
-export default function Page() {
+export default async function Page() {
+  const { totalTodos, completedTodos, todosMonth, todosWeek, todosDay } = await getDataTodo()
   return (
-    <TodoFormContextProvider>
-      <div className='flex flex-col gap-2'>
-        <div className='flex items-end justify-between p-6'>
-          <h1 className='text-3xl font-bold leading-[1.875rem]'>Dashboard</h1>
-        </div>
-
-        <div className='grid grid-cols-2 gap-6'>
-          <SummaryCard value='278' title='Total de Todos' icon={<LucideListChecks />} />
-          <SummaryCard value='99' icon={<LucideCheckCheck />} title='Total de Todos Completados' />
-        </div>
-        <h1 className='Text-xl font-bold'>Todos Completados</h1>
-        <div className='grid grid-cols-3 gap-6'>
-          <SummaryCard value='34' title='Total de Todos Mês' icon={<LucideCalendarCheck />} />
-          <SummaryCard value='10' icon={<LucideTally5 />} title='Total de Todos Semana' />
-          <SummaryCard value='1' title='Total de Todos Hoje' icon={<LucideSquareStack />} />
-        </div>
+    <div className='flex flex-col gap-2'>
+      <div className='flex items-end justify-between p-6'>
+        <h1 className='text-3xl font-bold leading-[1.875rem]'>Dashboard</h1>
       </div>
-    </TodoFormContextProvider>
+
+      <div className='grid grid-cols-2 gap-6'>
+        <SummaryCard value={totalTodos} title='Total de Todos' icon={<LucideListChecks />} />
+        <SummaryCard
+          value={completedTodos}
+          icon={<LucideCheckCheck />}
+          title='Total de Todos Completados'
+        />
+      </div>
+      <h1 className='Text-xl font-bold'>Todos Completados</h1>
+      <div className='grid grid-cols-3 gap-6'>
+        <SummaryCard
+          value={todosMonth.length}
+          title='Total de Todos Mês'
+          icon={<LucideCalendarCheck />}
+        />
+        <SummaryCard
+          icon={<LucideTally5 />}
+          value={todosWeek.length}
+          title='Total de Todos Semana'
+        />
+        <SummaryCard
+          value={todosDay.length}
+          title='Total de Todos Hoje'
+          icon={<LucideSquareStack />}
+        />
+      </div>
+    </div>
   )
 }
