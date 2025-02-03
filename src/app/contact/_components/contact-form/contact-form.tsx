487ef7cf41d547398aa9fa46d4f9618017edf6f7
marcos <dev.marcosanjos@gmail.com>
2025-02-02 22:31:05 -0300

    feat: refactor contact form component by removing unused state and handlers

--- src/app/contact/_components/contact-form/contact-form.tsx
+++ src/app/contact/_components/contact-form/contact-form.tsx
@@ -1,29 +1,27 @@
 'use client'
 
-import {useState} from 'react'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
-import {emailFormApi} from '@/apis/email-form'
-import {EmailFormData} from '@/types/utilities'
+import { EmailFormData } from '@/types/utilities'
 
 export const ContactForm = () => {
-  const {register, handleSubmit} = useForm<EmailFormData>()
+  const { register } = useForm<EmailFormData>()
 
-  const [isLoading, setIsLoading] = useState(false)
-  const [messageSucceeded, setMessageSucceeded] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [messageSucceeded, setMessageSucceeded] = useState(false)
 
-  const onSubmit = async (data: EmailFormData) => {
-    setIsLoading(true)
-    await emailFormApi.sendContactForm(data)
-    setIsLoading(false)
-    setMessageSucceeded(true)
-  }
+  // const onSubmit = async (data: EmailFormData) => {
+  //   setIsLoading(true)
+  //   await emailFormApi.sendContactForm(data)
+  //   setIsLoading(false)
+  //   setMessageSucceeded(true)
+  // }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='mx-auto flex w-full flex-col gap-5'>
+    <form className='mx-auto flex w-full flex-col gap-5'>
       <div className='flex flex-col gap-3 sm:flex-row'>
         <input
-          {...register('name', {required: true})}
+          {...register('name', { required: true })}
           type='text'
           placeholder='Your Name'
           className='input input-bordered w-full text-black placeholder:text-black/70'
@@ -31,25 +29,26 @@ export const ContactForm = () => {
       </div>
 
       <input
-        {...register('email', {required: true})}
+        {...register('email', { required: true })}
         type='text'
         placeholder='Your Email'
         className='input input-bordered w-full text-black placeholder:text-black/70'
       />
 
       <textarea
-        {...register('message', {required: true})}
+        {...register('message', { required: true })}
         placeholder='Type your message'
         className='textarea textarea-bordered w-full text-black placeholder:text-black/70'
       />
 
       <div className='flex flex-col items-center justify-center gap-2 md:flex-row md:gap-4'>
-        <button type='submit' disabled={isLoading} className='btn btn-primary btn-block'>
-          {isLoading ? <div className='loading loading-spinner' /> : 'Send Message'}
+        <button disabled type='submit' className='btn btn-primary btn-block'>
+          {/* {isLoading ? <div className='loading loading-spinner' /> : 'Send Message'} */}
+          Enviar
         </button>
       </div>
 
-      {messageSucceeded && <div className='alert alert-success'>Message send successfully</div>}
+      {/* {messageSucceeded && <div className='alert alert-success'>Message send successfully</div>} */}
     </form>
   )
 }
