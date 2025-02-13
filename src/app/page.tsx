969464bdcbe26498096d52461183e9c15eddd749
marcos <dev.marcosanjos@gmail.com>
2025-02-13 16:18:35 -0300

    feat: update layout padding for improved responsiveness on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Footer, Header, Hero } from '@/app/_components'
 export default async function Home() {
   return (
     <div className='container mx-auto flex min-h-screen w-full flex-col'>
-      <div className='flex  w-full flex-col items-center sm:mx-11'>
+      <div className='flex w-full flex-col items-center px-4 sm:px-6 md:px-10'>
         <Header />
         <Hero />
         <Price />
