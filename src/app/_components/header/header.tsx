b2bb147870ad9794d9c016c54304d4e33bebd563
marcos <dev.marcosanjos@gmail.com>
2025-02-13 16:15:09 -0300

    feat: update header component to include login link with navigation

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import Link from 'next/link'
+
 import { AudioWaveform } from 'lucide-react'
 
 export const Header = () => {
   return (
-    <header className='flex w-full justify-center bg-transparent p-2 sm:p-4'>
+    <header className='flex w-full justify-center bg-transparent p-2 sm:mx-5 sm:p-4'>
       <div className='container flex w-full items-center justify-between gap-6 md:gap-8'>
         <div className='max-sm:hidden sm:flex-initial'>
           <ul className='flex gap-4 *:text-primary-content'>
@@ -24,9 +26,9 @@ export const Header = () => {
           <h1 className='text-5xl font-extralight text-primary-content'>Bluestash</h1>
         </div>
         <div className='btn btn-primary'>
-          {/* <Link href='/auth' className='text-primary-content'> */}
-          Login
-          {/* </Link> */}
+          <Link href='/auth' className='text-primary-content'>
+            Login
+          </Link>
         </div>
       </div>
     </header>
