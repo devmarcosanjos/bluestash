87c2fc27bb13631ff850df1aff399e300a66d08b
marcos <dev.marcosanjos@gmail.com>
2025-02-13 16:15:29 -0300

    feat: update hero component layout and content for better clarity and responsiveness

--- src/app/_components/hero/hero.tsx
+++ src/app/_components/hero/hero.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export const Hero = () => {
   return (
-    <div className='container grid gap-4 text-wrap p-2 py-20 text-center'>
-      <p className='text-/6 justify-center font-bold text-primary-content'>É totalmente grátis</p>
+    <div className='container mx-5 grid gap-4 text-wrap p-2 py-20 text-center'>
+      <p className='text-/6 justify-center font-bold text-primary-content'>7 dias grátis</p>
       <h1 className='text-2xl text-primary-content md:text-4xl lg:text-6xl'>
         Transforme suas ideias em ações. Organize seu dia, conquiste seus objetivos.
       </h1>
@@ -15,8 +15,8 @@ export const Hero = () => {
           alt=''
           width={500}
           height={300}
-          className='rounded-lg'
           src='/bluestash-ladingpage.png'
+          className='h-auto max-w-[100%] rounded-lg object-cover lg:max-w-[500px]'
         />
       </div>
     </div>
