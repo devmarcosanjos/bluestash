7b60741f02e1d156738384a99b6c520537daad88
marcos <dev.marcosanjos@gmail.com>
2025-02-14 20:07:44 -0300

    feat: add data-theme attribute to multiple components for consistent theming

--- src/app/admin/dashboard/_components/summary-card.tsx
+++ src/app/admin/dashboard/_components/summary-card.tsx
@@ -6,7 +6,7 @@ interface SummaryCardProps {
 
 const SummaryCard = ({ icon, title, value }: SummaryCardProps) => {
   return (
-    <div ata-theme='blueStash' className='rounded-xl bg-neutral p-4'>
+    <div data-theme='blueStash' className='rounded-xl bg-neutral p-4'>
       <div className='mb-2 flex h-9 w-9 items-center justify-center rounded-md bg-primary-content bg-opacity-10 text-primary-content'>
         {icon}
       </div>
