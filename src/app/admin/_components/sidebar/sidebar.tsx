d0d280d50468436bb6cc725a93abe2592436c520
marcos <dev.marcosanjos@gmail.com>
2025-02-14 20:24:57 -0300

    feat: add logout functionality to sidebar component

--- src/app/admin/_components/sidebar/sidebar.tsx
+++ src/app/admin/_components/sidebar/sidebar.tsx
@@ -1,15 +1,22 @@
 'use client'
 
-import { usePathname, useRouter } from 'next/navigation'
+import { redirect, usePathname, useRouter } from 'next/navigation'
 
 import { Settings } from 'luxon'
-import { AudioWaveform, ChartColumnStacked, ListCheckIcon } from 'lucide-react'
+import { AudioWaveform, ChartColumnStacked, ListCheckIcon, LogOut } from 'lucide-react'
+
+import { supabase } from '@/libs/supabase/supabase-client'
 
 Settings.defaultLocale = 'pt-BR'
 
 const Sidebar = () => {
   const router = useRouter()
   const pathname = usePathname()
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+    redirect('/')
+  }
 
   const isActive = (path: string) => (pathname === path ? 'bg-primary text-white' : '')
 
@@ -22,22 +29,30 @@ const Sidebar = () => {
   }
 
   return (
-    <aside data-theme='blueStash' className=' w-64 p-4 text-primary-content '>
-      <div className='mb-16 mt-5 flex justify-start gap-4'>
+    <aside data-theme='blueStash' className=' flex w-64 flex-col p-4 text-primary-content'>
+      <div className='mb-16 mt-5 justify-start gap-4'>
         <AudioWaveform className='h-8 w-8 font-bold text-primary' />
         <h1 className='text-4xl font-extralight text-primary-content'>Bluestash</h1>
       </div>
 
-      <div className='flex flex-col space-y-4'>
-        <div
-          className={`flex cursor-pointer items-center gap-4 rounded-xl p-3 ${isActive('/admin')}`}>
-          <ListCheckIcon size={16} />
-          <button onClick={() => handlePathHome()}>TodoList</button>
+      <div className='flex flex-1 flex-col justify-between'>
+        <div className='space-y-4'>
+          <div
+            className={`flex cursor-pointer items-center gap-4 rounded-xl p-3 ${isActive('/admin')}`}>
+            <ListCheckIcon size={16} />
+            <button onClick={() => handlePathHome()}>TodoList</button>
+          </div>
+          <div
+            className={`flex cursor-pointer items-center gap-4 rounded-xl p-3 ${isActive('/admin/dashboard')}`}>
+            <ChartColumnStacked size={16} />
+            <button onClick={() => handlePathDashboard()}>Dashboard</button>
+          </div>
         </div>
+
         <div
-          className={`flex cursor-pointer items-center gap-4 rounded-xl p-3 ${isActive('/admin/dashboard')}`}>
-          <ChartColumnStacked size={16} />
-          <button onClick={() => handlePathDashboard()}>Dashboard</button>
+          className={`flex cursor-pointer items-center  gap-4 rounded-xl p-3 text-error hover:text-red-500`}>
+          <LogOut size={18} />
+          <button onClick={handleLogout}>Logout</button>
         </div>
       </div>
     </aside>
